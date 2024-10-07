d15864421c39aae152a895a782aa4b076e71e216
cmvanhov <cmvanhov@iu.edu>
2024-10-07 19:32:16 -0400

    fixed the date, and added a new activity, and also fixed the constraints for some things

--- app/src/main/java/com/example/calorie_counter_app/MainActivity.kt
+++ app/src/main/java/com/example/calorie_counter_app/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.calorie_counter_app
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dateValue: TextView
+    private lateinit var dateValue: EditText
     private lateinit var mealsValue: TextView
     private lateinit var waterValue: TextView
     private lateinit var snacksValue: TextView
@@ -18,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mealsUpdateButton: Button
     private lateinit var waterUpdateButton: Button
     private lateinit var snacksUpdateButton: Button
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,6 +39,28 @@ class MainActivity : AppCompatActivity() {
         mealsUpdateButton = findViewById(R.id.mealsUpdateButton)
         waterUpdateButton = findViewById(R.id.waterUpdateButton)
         snacksUpdateButton = findViewById(R.id.snacksUpdateButton)
+
+        mealsUpdateButton.setOnClickListener {
+            val intent = Intent(this, UpdateActivity::class.java)
+            startActivity(intent)
+        }
+        waterUpdateButton.setOnClickListener {
+            val intent = Intent(this, UpdateActivity::class.java)
+            startActivity(intent)
+        }
+        snacksUpdateButton.setOnClickListener {
+            val intent = Intent(this, UpdateActivity::class.java)
+            startActivity(intent)
+        }
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+        val formattedDate = current.format(formatter)
+        dateValue.setText(formattedDate)
+
+
+
+
 
     }
 }
